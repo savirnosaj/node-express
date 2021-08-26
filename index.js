1fe1310257484b5a82954914b268294825b95d9b
savir_nosaj <jason.rivas@bellevuecollege.edu>
2021-08-26 01:05:02 -0700

    update

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+var express = require('express');
+var data = require('./data/test.json');
+
+var app = express();
+const PORT = 3000;
+
+app.set('view engine','ejs');//sent views to EJS
+
+//this is how a route is built in Express
+//home page
+app.get('/', (req,res)=>{
+  var title = "Our Home Page";
+  res.render('pages/index',{title:title});
+});
+
+//add users route
+// users list page
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+// user/view route - we are cheating by using the array index + 1
+app.get('/users/view/:id', function(req, res) {
+  var title = 'User Page';
+  var id = req.params.id;
+  res.render('users/view', {
+      title: title,
+      user: data[--id]
+  });
+ });
+ 
+
+
+app.listen(PORT,()=>{
+  console.log(`App is running on port ${PORT}`);
+  console.log(data);
+});
